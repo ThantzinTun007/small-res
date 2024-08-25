a04ffffcf18d48bd6fdd5ed94754309079b7ce0a
ThantzinTun007 <tzt.thantzintun2022@gmail.com>
2024-08-25 22:57:38 +0630

    add register form and change css for home

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { RestaurantService } from '../../service/restaurant.service';
+import { Router } from '@angular/router';
+import { response } from 'express';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+  employee: any = {
+    name: '',
+    position: '',
+    salary: '',
+    hire_date: '',
+    phone: '',
+    email: '',
+    password: '',
+    role: 0,
+  };
 
+  constructor(private service: RestaurantService, private router: Router) {}
+
+  register() {
+    this.service.register(this.employee).subscribe(
+      (response) => {
+        console.log(response);
+        alert('Register successfully!');
+        this.employee = {};
+        this.router.navigate(['/register']);
+      },
+      (error) => {
+        console.log(error.message);
+      }
+    );
+  }
 }
